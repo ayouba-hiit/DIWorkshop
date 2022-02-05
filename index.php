13691abcc230ebff5a2ab5d952696db73fc8cf03
AyoubAid <abouaimad.ayoub@gmail.com>
2022-02-05 21:28:09 +0100

    03-Autowiring

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 use App\Controller\OrderController;
 use App\Database\Database;
 use App\Mailer\GmailMailer;
+use App\Mailer\SmtpMailer;
 use App\Texter\SmsTexter;
+use App\Texter\FaxTexter;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
@@ -11,25 +13,43 @@ require __DIR__ . '/vendor/autoload.php';
 
 $container = new ContainerBuilder();
 
-$container->register('order_controller', OrderController::class)
-    ->addArgument(new Reference('database'))
-    ->addArgument(new Reference('mailer.gmail'))
-    ->addArgument(new Reference('texter.sms'))
-    ->addMethodCall('sayHello', ['Salut IAD !!']);
+$container->autowire('order_controller', OrderController::class)
+    ->setPublic(true)
+    ->addMethodCall('sayHello', ['Salut IAD !!'])
+;
 
-$container->register('database', Database::class);
+$container->autowire('database', Database::class);
 
-$container->register('texter.sms', SmsTexter::class)
+$container->autowire('texter.sms', SmsTexter::class)
     ->addArgument("service.sms.com")
     ->addArgument("apikey123")
 ;
 
-$container->register('mailer.gmail', GmailMailer::class)
+$container->autowire('texter.fax', FaxTexter::class);
+
+$container->autowire('mailer.gmail', GmailMailer::class)
     ->addArgument("ayoub@gmail.com")
     ->addArgument("123456")
 ;
 
-$controller = $container->get('order_controller');
+$container->autowire('smtp.gmail', SmtpMailer::class)
+    ->addArgument("smtp://localhost")
+    ->addArgument("root")
+    ->addArgument('123')
+;
+
+$container->setAlias('App\Controller\OrderController', 'order_controller')->setPublic(true);
+$container->setAlias('App\Database\Database', 'database');
+$container->setAlias('App\Mailer\GmailMailer', 'mailer.gmail');
+$container->setAlias('App\Mailer\SmtpMailer', 'mailer.smtp');
+$container->setAlias('App\Texter\SmsTexter', 'texter.sms');
+$container->setAlias('App\Texter\FaxTexter', 'texter.fax');
+$container->setAlias('App\Texter\TexterInterface', 'texter.sms');
+$container->setAlias('App\Mailer\MailerInterface', 'mailer.gmail');
+
+$container->compile();
+
+$controller = $container->get(OrderController::class);
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
